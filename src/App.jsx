f9a87d5d98f44b263e202cc01a8ec0c37a102b28
mukundhasuresh <mukundhasuresh@gmail.com>
2026-02-19 09:32:31 +0530

    feat: protected dashboard routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
