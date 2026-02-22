b3a27e79e22617c5f81c28f7520b78d25dbb3e41
mukundhasuresh <mukundhasuresh@gmail.com>
2026-02-22 17:35:38 +0530

    chore: production API config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://branch-loan-backend.onrender.com",
   withCredentials: true,
 });
 
